52081c2fe677eeca3b16ad8591a43ca52de9cabf
unkuwun_x3 <joaogabrielrodrigues95@gmail.com>
2026-09-23 19:52:23 -0300

    test stuff

--- ruby_on_rails/test.rb
+++ ruby_on_rails/test.rb
@@ -1,14 +1,7 @@
-class Names
-  def initialize(name, name_2)
-    @name = name
-    @name_2 = name_2
-  end
-
-  def greet
-    puts "\nHello, #{@name} and #{@name_2}!"
-  end
+class Teste
+  puts segundo
 end
 
-names_1 = Names.new("João", "Maria")
-
-puts names_1.greet 
+def segundo
+    "Olá"
+end
